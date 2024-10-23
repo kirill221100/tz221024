ba1ed770e999a23c023eda457f516c2fe1292df9
kirill221100 <kirulkacool@gmail.com>
2024-10-23 17:56:06 +0700

    init

--- utils/auth.py
+++ utils/auth.py
@@ -8,8 +8,10 @@ from security.jwt import create_access_token
 
 async def auth_func(auth_data: OAuth2PasswordRequestForm, session: AsyncSession):
     user = await get_user_by_username(auth_data.username, session)
-    if verify_pass(auth_data.password, user.hashed_password):
-        data = {'id': user.id}
-        access_token = await create_access_token(data)
-        return {'access_token': access_token, 'token_type': 'bearer'}
-    raise HTTPException(401, 'Неправильное имя или пароль')
+    if user:
+        if verify_pass(auth_data.password, user.hashed_password):
+            data = {'id': user.id}
+            access_token = await create_access_token(data)
+            return {'access_token': access_token, 'token_type': 'bearer'}
+        raise HTTPException(401, 'Неправильное имя или пароль')
+    raise HTTPException(404, 'Пользователя с таким именем не существует')
